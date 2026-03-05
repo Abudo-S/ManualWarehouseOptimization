683a75daa8209e1bc72436d2ee8f393fcd3d43f0
Abudo-S <abudo.mohamed123456789@gmail.com>
2026-03-05 16:30:06 +0100

    Adjust GnnScheduleDataset for unlabelled batches.

--- GnnScheduleDataset.py
+++ GnnScheduleDataset.py
@@ -8,10 +8,14 @@ from GnnDataInstanceBuilder import GnnDataInstanceBuilder
 from MultiCriteriaGNNModel import MultiCriteriaGNNModel
 
 
+LARGE_SCALE_BATCH_NAME = "Batch1000M" #Batch1000M, Batch9000M or Batch10000M
 #file paths
-MISSION_BATCH_DIR = "./datasets/mini-batch/Batch10M_distanced.csv"
+LARGE_BATCH_DIR = "./datasets/large-batch/batch/"
+MISSION_LARGE_BATCH_DIR = "./datasets/large-batch/batch/Batch_1_100M_distanced_A1.0_B1000.0_H90.csv"
+MISSION_BATCH_DIR = f"./datasets/{LARGE_SCALE_BATCH_NAME}/mini-batch/Batch10M_distanced.csv"
 UDC_TYPES_DIR = "./datasets/WM_UDC_TYPE.csv"
-MISSION_BATCH_TRAVEL_DIR = "./datasets/mini-batch/Batch10M_travel_distanced.csv"
+MISSION_LARGE_BATCH_TRAVEL_DIR = "./datasets/large-batch/travel/Batch_1_100M_travel_distanced.csv"
+MISSION_BATCH_TRAVEL_DIR = f"./datasets/{LARGE_SCALE_BATCH_NAME}/mini-batch/Batch10M_travel_distanced.csv"
 FORK_LIFTS_DIR = "./datasets/ForkLifts10W.csv"
 #MISSION_TYPES_DIR = "./datasets/MissionTypes.csv"
 SCHEDULE_DIR = "./schedules/mini-batch/"
@@ -26,43 +30,75 @@ class GnnScheduleDataset(Dataset):
                 mission_base_path, 
                 edge_base_path, 
                 pallet_types_file_path, 
-                fork_path):
+                fork_path,
+                large_batch_dir=None):
         
         self.schedule_dir = schedule_dir
+        self.large_batch_dir = large_batch_dir
+
         self.builder = GnnDataInstanceBuilder()
         self.pallet_types_file_path = pallet_types_file_path
         self.fork_path = fork_path
         
         #discover all schedule files
         #pattern: schedule..._1_A...B...H...0.csv
-        search_pattern = os.path.join(schedule_dir, "schedule*0.csv")
-        all_schedules = sorted(glob.glob(search_pattern))
-        
-        self.items = []
-        
-        #match each schedule to its batch files
-        #assumes filenames like: ..._1_... matches Batch10M_..._1.csv
-        for sched_path in all_schedules:
-            filename = os.path.basename(sched_path)
+        if schedule_dir:
+            search_pattern = os.path.join(schedule_dir, "schedule*0.csv")
+            all_schedules = sorted(glob.glob(search_pattern))
             
-            #extract batch number (e.g. '1' from 'schedule10M_1_A...')
-            match = re.search(r'_(\d+)_A', filename) 
-            if match:
-                batch_num = match.group(1)
+            self.items = []
+            
+            #match each schedule to its batch files
+            #assumes filenames like: ..._1_... matches Batch10M_..._1.csv
+            for sched_path in all_schedules:
+                filename = os.path.basename(sched_path)
                 
-                #add corresponding paths
-                node_path = mission_base_path.replace('.csv', f'_{batch_num}.csv')
-                edge_path = edge_base_path.replace('.csv', f'_{batch_num}.csv')
+                #extract batch number (e.g. '1' from 'schedule10M_1_A...')
+                match = re.search(r'_(\d+)_A', filename) 
+                if match:
+                    batch_num = match.group(1)
+                    
+                    #add corresponding paths
+                    node_path = mission_base_path.replace('.csv', f'_{batch_num}.csv')
+                    edge_path = edge_base_path.replace('.csv', f'_{batch_num}.csv')
+                    
+                    if os.path.exists(node_path) and os.path.exists(edge_path):
+                        self.items.append({
+                            'schedule': sched_path,
+                            'node': node_path,
+                            'edge': edge_path,
+                            'id': batch_num
+                        })
+                    else:
+                        print(f"Warning: Missing node/edge files for schedule {filename}")
+        else: #large-scale (test) batches don't have any ground-truth schedules
+            search_pattern = os.path.join(large_batch_dir, "*_distanced_*")
+            all_batches = sorted(glob.glob(search_pattern))
+            
+            self.items = []
+            
+            #match each batch to its travel files
+            #assumes filenames like: ..._1 matches Batch_1_100M_distanced_A1.0_B1000.0_H90.csv
+            for batch_path in all_batches:
+                filename = os.path.basename(batch_path)
                 
-                if os.path.exists(node_path) and os.path.exists(edge_path):
-                    self.items.append({
-                        'schedule': sched_path,
-                        'node': node_path,
-                        'edge': edge_path,
-                        'id': batch_num
-                    })
-                else:
-                    print(f"Warning: Missing node/edge files for schedule {filename}")
+                #extract batch number (e.g. '1' from 'Batch_1_...')
+                match = re.search(r'h_(\d+)_', filename) 
+                if match:
+                    batch_num = match.group(1)
+                    
+                    #add corresponding paths
+                    node_path = os.path.join(large_batch_dir, filename)
+                    edge_path = edge_base_path.replace('Batch_1', f'Batch_{batch_num}')
+                    
+                    if os.path.exists(node_path) and os.path.exists(edge_path):
+                        self.items.append({
+                            'node': node_path,
+                            'edge': edge_path,
+                            'id': batch_num
+                        })
+                    else:
+                        print(f"Warning: Missing node/edge files for batch {filename}")
 
     def __len__(self):
         return len(self.items)
@@ -76,7 +112,7 @@ class GnnScheduleDataset(Dataset):
             pallet_types_file_path=self.pallet_types_file_path,
             operator_file_path=self.fork_path,
             edge_file_path=item['edge'],
-            schedule_file_path=item['schedule']
+            schedule_file_path=item.get('schedule', None)
         )
         
         return data
@@ -85,13 +121,23 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
 
-    #init Dataset
+    #init mini-batch dataset
+    # dataset = GnnScheduleDataset(
+    #     schedule_dir=SCHEDULE_DIR,
+    #     mission_base_path=MISSION_BATCH_DIR,
+    #     edge_base_path=MISSION_BATCH_TRAVEL_DIR,
+    #     pallet_types_file_path=UDC_TYPES_DIR,
+    #     fork_path=FORK_LIFTS_DIR
+    # )
+
+    #init large-batch dataset
     dataset = GnnScheduleDataset(
-        schedule_dir=SCHEDULE_DIR,
-        mission_base_path=MISSION_BATCH_DIR,
-        edge_base_path=MISSION_BATCH_TRAVEL_DIR,
+        schedule_dir=None,
+        mission_base_path=MISSION_LARGE_BATCH_DIR,
+        edge_base_path=MISSION_LARGE_BATCH_TRAVEL_DIR,
         pallet_types_file_path=UDC_TYPES_DIR,
-        fork_path=FORK_LIFTS_DIR
+        fork_path=FORK_LIFTS_DIR,
+        large_batch_dir=LARGE_BATCH_DIR
     )
     
     print(f"Found {len(dataset)} valid schedule instances.")
